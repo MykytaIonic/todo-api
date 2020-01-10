431791b229d373da44227b040b281382342e9655
MykytaIonic <mykytarusynyk@gmail.com>
2020-01-10 12:00:55 +0200

    commit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  var fs = require('fs');
+  var dir = './photos';
+
+  if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+  }
   app.use('/photos', express.static('photos'));
   await app.listen(3000);
 }
